7849579a875a9178cdcf0f70eb32f535611ccf9f
ashrithr <ashrith@me.com>
2013-07-20 21:12:13 -0700

    Updates Utilities to support ubuntu uses puppetlabs_apt to pull apt sources, Fixes Issue #5

--- utilities/manifests/init.pp
+++ utilities/manifests/init.pp
@@ -1,7 +1,25 @@
 class utilities {
   case $operatingsystem {
     'Ubuntu': {
-      #TODO
+      include apt
+      apt::source { "cloudera_precise":
+        location        => "[arch=${::architecture}] http://archive.cloudera.com/cdh4/ubuntu/${::lsbdistcodename}/${::architecture}/cdh",
+        release         => "${::lsbdistcodename}-cdh4",
+        repos           => " contrib",
+        include_src     => true
+      }
+      apt::key { 'cloudera_precise':
+        key        => 'D50582E6',
+        key_source => "http://archive.cloudera.com/cdh4/ubuntu/${::lsbdistcodename}/${::architecture}/cdh/archive.key",
+        before => Apt::Source['cloudera_precise'],
+      }
+      apt::source { "cloudera_impala_precise":
+        location        => "[arch=amd64] http://archive.cloudera.com/impala/ubuntu/precise/amd64/impala",
+        release         => "precise-impala1",
+        repos           => " contrib",
+        include_src     => true,
+        require         => Apt::Source['cloudera_precise']
+      }
     }
     'CentOS': {
       yumrepo { "cloudera-repo":
@@ -24,8 +42,10 @@ class utilities {
         refreshonly => true
       }
     }
+    default:  {fail('Supported OS are CentOS, Ubuntu')}
   }
-  #log_stash
+
+  #Add lubmerjack_default to monitor default config files based on OS
   $log_aggregation = hiera('log_aggregation', 'disabled')
 
   if ($log_aggregation == 'enabled') {
