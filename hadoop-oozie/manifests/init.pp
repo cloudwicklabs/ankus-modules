3ae30e8d4de10e3b50d0df45cf5e377f359aaf75
ashrithr <ashrith@me.com>
2013-07-22 23:47:33 -0700

    Updates oozie module with oozie-env inpath

--- hadoop-oozie/manifests/init.pp
+++ hadoop-oozie/manifests/init.pp
@@ -1,6 +1,12 @@
-# == Class: hadoop-oozie
+# == Class:
+# hadoop-oozie::server
+# hadoop-oozie::client
 #
-# This module installs and manages oozie, which is a workflow scheduler system to manage Hadoop jobs.
+# This module installs and manages oozie, which is a workflow scheduler system to manage Hadoop jobs
+# (MapReduce, Streaming, Pipes, Pig, Hive, Sqoop, etc).
+#
+# Oozie client is a command-line utility that interacts with the Oozie server via the Oozie
+# web-services API
 #
 # === Parameters
 #
@@ -58,8 +64,17 @@ class hadoop-oozie {
     }
 
     file { "/etc/oozie/conf/oozie-site.xml":
+      alias   => 'oozie-conf',
       content => template("hadoop-oozie/oozie-site.xml.erb"),
       require => Package["oozie"],
+      notify  => Service["oozie"]
+    }
+
+    file { "/etc/oozie/conf/oozie-env.sh":
+      alias   => 'oozie-env',
+      content => template("hadoop-oozie/oozie-env.sh.erb"),
+      require => Package["oozie"],
+      notify  => Service["oozie"]
     }
 
     exec { "Oozie DB init":
@@ -71,12 +86,14 @@ class hadoop-oozie {
 
     service { "oozie":
       ensure => running,
-      require => [ Package["oozie"], Exec["Oozie DB init"] ],
+      require => [ Package["oozie"], Exec["Oozie DB init"], File["oozie-conf"], File["oozie-env"] ],
       hasrestart => true,
       hasstatus => true,
     }
 
-    #oozie web interface
+    #
+    # Oozie web interface
+    #
     file { "/var/lib/oozie/ext-2.2.tar.gz":
       source => "puppet:///modules/hadoop-oozie/ext-2.2.tar.gz",
       alias => "ext-source-tgz",
@@ -92,7 +109,13 @@ class hadoop-oozie {
       subscribe => File["ext-source-tgz"],
       require => File["ext-source-tgz"],
     }
-    #FIX: This fails (datanodes should be up and running to do this.)
+
+    #
+    # Install Oozie ShareLin in HDFS - contains all of the necessary JARs to enable workflow jobs to run
+    #                                  streaming, DistCp, Pig, Hive, and Sqoop actions.
+    #
+
+    #FIX ME: This fails (datanodes should be up and running to do this.)
     #exec { "untar-oozie-share-lib":
     #  command => "/bin/tar -xzf oozie-sharelib.tar.gz",
     #  cwd => "/usr/lib/oozie",
