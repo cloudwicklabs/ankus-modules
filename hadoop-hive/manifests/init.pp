78d63440a301420e5fc0ab29a96a93fb6b8b0de1
ashrithr <ashrith@me.com>
2013-07-22 19:02:41 -0700

    Updates hive module to include hive metastore service

--- hadoop-hive/manifests/init.pp
+++ hadoop-hive/manifests/init.pp
@@ -1,6 +1,10 @@
 # == Class: hadoop-hive
 #
-# This module installs and manages hive, which is a data warehouse system for Hadoop
+# This module installs and manages:
+#   - hive: which is a data warehouse system for Hadoop
+#   - hive-metastore: stores the metadata for Hive tables and partitions in a relational database,
+#                     and provides clients (including Hive) access to this information via the
+#                     metastore service API
 #
 # === Parameters
 #
@@ -8,9 +12,22 @@
 #
 # === Variables
 #
-# [*hbase_hive_integration*]
+# [*hbase_install*]
 #   specifies whether to integrate hive and hbase
 #   possible values -> enabled | disabled
+#
+# [*hadoop_controller*]
+#   specifies the controller, host on which postgresql is running
+#
+# [*impala*]
+#   specifies whether to integrate hive and impala
+#   possible values -> enabled | disabled
+#
+# [*hbase_master*]
+#   fqdn of hbase master if hbase is enabled
+#
+# [*hbase_zookeeper_quorum*]
+#   zookeeper quorum (list of zookeeper servers used by hbase service)
 #
 # === Requires
 #
@@ -31,12 +48,10 @@ class hadoop-hive {
   $hbase_install = hiera('hbase_install')
   $hadoop_controller = hiera('controller')
   $impala = hiera('impala', 'disabled')
-
 	if ($hbase_install == "enabled"){
 		$hbase_master = hiera('hbase_master')
 		$hbase_zookeeper_quorum = hiera('zookeeper_quorum')
 	}
-
   $hive_packages = [ "hive", "hive-metastore" ]
 
   package { $hive_packages:
@@ -54,6 +69,14 @@ class hadoop-hive {
   file { "/etc/hive/conf/hive-site.xml":
     content => template('hadoop-hive/hive-site.xml.erb'),
     require => Package["hive"],
+    alias   => 'hive-conf',
+    notify  => Service['hive-metastore'],
+  }
+
+  service { "hive-metastore":
+    enable => true,
+    ensure => running,
+    require => File['hive-conf'],
   }
 
   # if( $impala == "enabled" ) {
