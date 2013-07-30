a63d88d1188d4147e61d822793a71a809a76113e
ashrithr <ashrith@me.com>
2013-07-30 16:45:35 -0700

    Includes cassandra deployment module

--- cassandra/manifests/init.pp
+++ cassandra/manifests/init.pp
@@ -4,7 +4,7 @@
 #
 # === Parameters
 #
-# None
+# cassandra::params
 #
 # === Variables
 #
@@ -12,7 +12,7 @@
 #
 # === Requires
 #
-# Nothing.
+# Java.
 #
 # === Sample Usage
 #
@@ -26,7 +26,6 @@
 #
 # Copyright 2012 Cloudwick Technologies, unless otherwise noted.
 #
-
 
 class cassandra {
 
@@ -76,117 +75,27 @@ class cassandra {
     }
   }
 
-
-  file {"$cassandra::params::cassandra_base":
-		ensure => "directory",
-		owner => "cassandra",
-		group => "cassandra",
-		alias => "cassandra-base",
-	}
-
-  file {"$cassandra::params::cassandra_log_path":
-		ensure => "directory",
-		owner => "cassandra",
-		group => "cassandra",
-		alias => "cassandra-log-path",
-        require => File["cassandra-base"]
-	}
-
-  file {"$cassandra::params::data_path":
-		ensure => "directory",
-		owner => "cassandra",
-		group => "cassandra",
-		alias => "cassandra-data-path",
-	}
-
-  file {"$cassandra::params::commitlog_directory":
-		ensure => "directory",
-		owner => "cassandra",
-		group => "cassandra",
-		alias => "cassandra-commitlog-directory",
-    require => File["cassandra-base"]
-	}
-
-  file {"$cassandra::params::saved_caches":
-		ensure => "directory",
-		owner => "cassandra",
-		group => "cassandra",
-		alias => "cassandra-saved-caches",
-        require => File["cassandra-base"]
-	}
-
-  file { "${cassandra::params::cassandra_base}/apache-cassandra-${cassandra::params::version}-bin.tar.gz":
-		mode => 0644,
-		owner => cassandra,
-		group => cassandra,
-		source => "puppet:///modules/cassandra/apache-cassandra-${cassandra::params::version}-bin.tar.gz",
-		alias => "cassandra-source-tgz",
-		before => Exec["untar-cassandra"],
-		require => File["cassandra-base"]
-	}
-
-  exec { "untar apache-cassandra-${cassandra::params::version}-bin.tar.gz":
-		command => "/bin/tar -zxf apache-cassandra-${cassandra::params::version}-bin.tar.gz",
-		cwd => "${cassandra::params::cassandra_base}",
-		creates => "${cassandra::params::cassandra_base}/cassandra-${cassandra::params::version}",
-		alias => "untar-cassandra",
-		refreshonly => true,
-		subscribe => File["cassandra-source-tgz"],
-		user => "cassandra",
-		before => [ File["cassandra-symlink"], File["cassandra-app-dir"]]
-	}
-
-  file { "${cassandra::params::cassandra_base}/apache-cassandra-${cassandra::params::version}":
-		ensure => "directory",
-		mode => 0644,
-		owner => "cassandra",
-		group => "cassandra",
-		alias => "cassandra-app-dir",
-                before => [ File["cassandra-yaml"], File["cassandra-log4j"], File["cassandra-env"] ]
-	}
-
-	file { "${cassandra::params::cassandra_base}/apache-cassandra":
-		force => true,
-		ensure => "${cassandra::params::cassandra_base}/apache-cassandra-${cassandra::params::version}",
-		alias => "cassandra-symlink",
-		owner => "cassandra",
-		group => "cassandra",
-		require => File["cassandra-source-tgz"],
-	}
-
-  file { "${cassandra::params::cassandra_base}/apache-cassandra-${cassandra::params::version}/conf/cassandra.yaml":
-          alias => "cassandra-yaml",
-          content => template("cassandra/conf/cassandra.yaml.erb"),
-          owner => "cassandra",
-          group => "cassandra",
-          mode => "644",
-          require => File["cassandra-app-dir"]
+  file { "${cassandra::params::cassandra_base}/conf/cassandra.yaml":
+    ensure  => present,
+    alias   => 'conf',
+    require => Package['dsc12'],
+    content => template("cassandra/conf/cassandra.yaml.erb"),
+    notify  => Service['cassandra'];
   }
 
-  file { "${cassandra::params::cassandra_base}/apache-cassandra-${cassandra::params::version}/conf/cassandra-env.sh":
-          alias => "cassandra-env",
-          content => template("cassandra/conf/cassandra-env.sh.erb"),
-          owner => "cassandra",
-          group => "cassandra",
-          mode => "644",
-          require => File["cassandra-app-dir"]
+  file {'${cassandra::params::cassandra_base}/conf/cassandra-env.sh':
+    ensure  => present,
+    alias   => 'conf-env',
+    require => Package['dsc12'],
+    content => template("cassandra/conf/cassandra-env.sh.erb"),
+    notify  => Service['cassandra'];
   }
 
-  file { "${cassandra::params::cassandra_base}/apache-cassandra-${cassandra::params::version}/conf/log4j-server.properties":
-          alias => "cassandra-log4j",
-          content => template("cassandra/conf/log4j-server.properties.erb"),
-          owner => "cassandra",
-          group => "cassandra",
-          mode => "644",
-          require => File["cassandra-app-dir"]
+  service { "cassandra":
+    enable => true,
+    ensure => running,
+    hasrestart => true,
+    hasstatus => true,
+    require => [Package['dsc12'], File['conf', 'conf-env']],
   }
-
-	file {"${cassandra::params::cassandra_base}/apache-cassandra-${cassandra::params::version}/lib/mx4j-tools.jar":
-		ensure => present,
-		source => "puppet:///modules/cassandra/mx4j-tools.jar",
-		owner => "cassandra",
-		group => "cassandra",
-		mode => "644",
-	}
-
 }
