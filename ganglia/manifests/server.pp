eb51df1a528c8952b702ab15bf1dad494f9a1a49
ashrithr <ashrith@me.com>
2013-08-01 21:13:09 -0700

    Tries to fix #3

--- ganglia/manifests/server.pp
+++ ganglia/manifests/server.pp
@@ -24,7 +24,8 @@ class ganglia::server (
     ensure  =>  running,
     enable  => true,
     status => 'ps aux | grep gmetad | grep -qv grep',
-    require => Package[$ganglia_server_pkg];
+    require => Package[$ganglia_server_pkg],
+    notify  => Exec['refresh-apache']
   }
 
   file {'/etc/ganglia/gmetad.conf':
