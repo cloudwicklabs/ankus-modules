25307c046d82b540bdba70220c264da8a3076e71
ashrithr <ashrith@me.com>
2013-08-26 21:18:09 -0700

    Fixes type for storm module init

--- storm/manifests/init.pp
+++ storm/manifests/init.pp
@@ -2,7 +2,7 @@ class storm(
   $nimbus_host      = hiera(storm_nimbus_host, "localhost"),
   $zookeeper_hosts  = hiera(zookeeper_quoram, ["localhost"]),
   $storm_local_dir  = hiera(storm_local_dir, "/var/lib/storm"),
-  $worker_count     = hiera(storm_local_dir, undef),
+  $worker_count     = hiera(storm_worker_count, undef),
   $ui_port          = hiera(storm_ui_port, 8080),
   $enable_jmxremote = true
   ) inherits storm::params {
