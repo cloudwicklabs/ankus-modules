87d378a607f2fc32597754101ec80034f30ab246
ashrithr <ashrith@me.com>
2014-03-12 11:55:25 -0700

    Updates mkdir_p custom provider

--- utils/lib/puppet/provider/mkdir_p/ruby.rb
+++ utils/lib/puppet/provider/mkdir_p/ruby.rb
@@ -6,8 +6,6 @@ Puppet::Type.type(:mkdir_p).provide(:ruby) do
   # 'exists', 'create' and 'destroy'
 
   def exists?
-    # should return a boolean specifying whether a resource exists on
-    # the system or not
     File.exists? resource[:name]
   end
 
@@ -15,9 +13,7 @@ Puppet::Type.type(:mkdir_p).provide(:ruby) do
     FileUtils.mkdir_p resource[:name]
   end
 
-
   def destroy
     FileUtils.rm_rf resource[:name]
   end
-
 end
