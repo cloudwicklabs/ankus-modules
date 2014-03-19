82933378283cdf836736dd0cdfbc46814365d1ae
ashrithr <ashrith@me.com>
2014-03-19 16:23:58 -0700

    [cm] Updates params.pp to include database related configuration parameters

--- cm/manifests/params.pp
+++ cm/manifests/params.pp
@@ -26,11 +26,12 @@
 # Copyright 2012 Cloudwick Technologies, unless otherwise noted.
 #
 class cm::params {
-
-  $cm_username = hiera('cloudera_manager_username', 'admin')
-  $cm_password = hiera('cloudera_manager_username', 'admin')
-  $cm_version  = hiera('cloudera_manager_version', 5)
-  $cm_api_port = hiera('cloduera_manager_api_port', '7180')
+  $cm_username     = hiera('cloudera_manager_username', 'admin')
+  $cm_password     = hiera('cloudera_manager_username', 'admin')
+  $cm_version      = hiera('cloudera_manager_version', 5)
+  $cm_api_port     = hiera('cloduera_manager_api_port', '7180')
+  $cm_cluster_name = hiera('cm_cluster_name', 'ankus_cluster')
+  $cm_cluster_ver  = hiera('cm_cluster_ver', 'CDH4')
 
   $repo_class = $::osfamily ? {
     /(?i-mx:redhat)/ => 'utils::repos::cm::yum',
@@ -75,4 +76,29 @@ class cm::params {
   } else {
     $safe_use_parcels = $use_parcels
   }
+
+  #scm database
+  $db_type = "postgresql"
+  $db_host = $::fqdn
+  $db_port = '5432'
+  $scm_database_name = hiera('cm_database_name', 'scm')
+  $scm_username = hiera('cm_database_user', 'scm')
+  $scm_password = hiera('cm_database_password', 'scm')
+
+  #management services user configuration
+  $cm_amon_database_name     = hiera('cm_amon_database_name', 'amon')
+  $cm_amon_database_user     = hiera('cm_amon_database_user', 'amon')
+  $cm_amon_listen_port       = hiera('cm_amon_listen_port', '9999')
+  $cm_amon_database_type     = hiera('cm_amon_database_type', 'postgresql')
+  $cm_amon_database_password = hiera('cm_amon_database_password', 'amon')
+  $cm_smon_database_name     = hiera('cm_smon_database_name', 'smon')
+  $cm_smon_database_user     = hiera('cm_smon_database_user', 'smon')
+  $cm_smon_listen_port       = hiera('cm_smon_listen_port', '9997')
+  $cm_smon_database_type     = hiera('cm_smon_database_type', 'postgresql')
+  $cm_smon_database_password = hiera('cm_smon_database_password', 'smon')
+  $cm_hmon_database_name     = hiera('cm_hmon_database_name', 'hmon')
+  $cm_hmon_database_user     = hiera('cm_hmon_database_user', 'hmon')
+  $cm_hmon_listen_port       = hiera('cm_hmon_listen_port', '9995')
+  $cm_hmon_database_type     = hiera('cm_hmon_database_type', 'postgresql')
+  $cm_hmon_database_password = hiera('cm_hmon_database_password', 'hmon')
 }
