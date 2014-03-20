f486db567041e6ce9f997598fd00c1b0daea59ef
ashrithr <ashrith@me.com>
2014-03-20 01:00:55 -0700

    [cm] Fixes typo in scm database config

--- cm/manifests/database.pp
+++ cm/manifests/database.pp
@@ -1,16 +1,16 @@
 # Creates scm database in postgres
-class cm::database {
-  postgres::server::db { $cm::params::scm_database_name:
-    user     => $scm_username,
-    password => $scm_password
+class cm::database inherits utils::database {
+  postgresql::server::db { $cm::params::scm_database_name:
+    user     => $cm::params::scm_username,
+    password => $cm::params::scm_password
   }
 
-  postgres::server::pg_hba_rule { 'allow scm user':
+  postgresql::server::pg_hba_rule { 'allow scm user':
     description => 'Open up postgresql for access to scm database',
     type        => 'host',
     database    => $cm::params::scm_database_name,
     user        => $cm::params::scm_username,
-    address     => '127.0.0.1/32',
+    address     => '0.0.0.0/0',
     auth_method => 'trust'
   }
 }
