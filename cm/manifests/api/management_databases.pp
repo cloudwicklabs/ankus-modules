33431151ba6c56a394f74be148109da996c71b66
ashrithr <ashrith@me.com>
2014-03-20 00:59:11 -0700

    [cm] Fixes typo in management databases config

--- cm/manifests/api/management_databases.pp
+++ cm/manifests/api/management_databases.pp
@@ -1,43 +1,43 @@
-class cm::api::management_databases {
-  postgres::server::db { $cm::params::cm_amon_database_name:
+class cm::api::management_databases inherits utils::database {
+  postgresql::server::db { $cm::params::cm_amon_database_name:
     user     => $cm::params::cm_amon_database_user,
     password => $cm::params::cm_amon_database_password
   }
 
-  postgres::server::pg_hba_rule { 'allow activity monitor user':
+  postgresql::server::pg_hba_rule { 'allow activity monitor user':
     description => 'Open up postgresql for access to activity monitor database',
     type        => 'host',
     database    => $cm::params::cm_amon_database_name,
     user        => $cm::params::cm_amon_database_user,
-    address     => '127.0.0.1/32',
+    address     => '0.0.0.0/0',
     auth_method => 'trust'
   }
 
-  postgres::server::db { $cm::params::cm_smon_database_name:
+  postgresql::server::db { $cm::params::cm_smon_database_name:
     user     => $cm::params::cm_smon_database_user,
     password => $cm::params::cm_smon_database_password
   }
 
-  postgres::server::pg_hba_rule { 'allow service monitor user':
+  postgresql::server::pg_hba_rule { 'allow service monitor user':
     description => 'Open up postgresql for access to service monitor database',
     type        => 'host',
     database    => $cm::params::cm_smon_database_name,
     user        => $cm::params::cm_smon_database_user,
-    address     => '127.0.0.1/32',
+    address     => '0.0.0.0/0',
     auth_method => 'trust'
   }
 
-  postgres::server::db { $cm::params::cm_hmon_database_name:
+  postgresql::server::db { $cm::params::cm_hmon_database_name:
     user     => $cm::params::cm_hmon_database_user,
     password => $cm::params::cm_hmon_database_password
   }
 
-  postgres::server::pg_hba_rule { 'allow host monitor user':
+  postgresql::server::pg_hba_rule { 'allow host monitor user':
     description => 'Open up postgresql for access to host monitor database',
     type        => 'host',
     database    => $cm::params::cm_hmon_database_name,
     user        => $cm::params::cm_hmon_database_user,
-    address     => '127.0.0.1/32',
+    address     => '0.0.0.0/0',
     auth_method => 'trust'
   }
 }
