619b1dd9cce8a151f4d4c266f06b2dd2a2bd4633
ashrithr <ashrith@me.com>
2014-03-20 01:00:05 -0700

    [cm] Updates `api::params` to include cluster name, for convience sake

--- cm/manifests/api/params.pp
+++ cm/manifests/api/params.pp
@@ -1,5 +1,6 @@
 class cm::api::params inherits cm::params {
   # find out the api version to work with
+  $cluster_name = $::cm::params::cm_cluster_name
   $cm_domain = "http://${::cm::params::cm_server_host}:${::cm::params::cm_api_port}"
   $api_version = inline_template("<%= require 'uri'
     open(\"${cm_domain}/api/version\",
